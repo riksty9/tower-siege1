ff277b2a9257a90a405101a3d29fc85cf98dd538
riksty9 <68648886+riksty9@users.noreply.github.com>
2020-10-02 12:25:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,109 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var polygon_img;
+var score=0;
+function preload(){
+  polygon_img=loadImage("polygon.png");
+}
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
+  box1 = new Box(300,275,30,40);
+  box2 = new Box(330,275,30,40);
+  box3 = new Box(360,275,30,40);
+  box4 = new Box(390,275,30,40);
+  box5 = new Box(420,275,30,40);
+  box6 = new Box(450,275,30,40);
+  box7 = new Box(480,275,30,40);
+  box8 = new Box(330,235,30,40);
+  box9 = new Box(360,235,30,40);
+  box10 = new Box(390,235,30,40);
+  box11 = new Box(420,235,30,40);
+  box12 = new Box(450,235,30,40);
+  box13 = new Box(360,195,30,40);
+  box14 = new Box(390,195,30,40);
+  box15 = new Box(420,195,30,40);
+  box16 = new Box(390,155,30,40);
+  box17 = new Box(675,180,30,40);
+  box18 = new Box(685,180,30,40);
+  box19 = new Box(695,180,30,40);
+  box20 = new Box(705,180,30,40);
+  box21 = new Box(715,180,30,40);
+  box22 = new Box(725,180,30,40);
+  box23 = new Box(685,150,30,40);
+  box24 = new Box(695,150,30,40);
+  box25 = new Box(705,150,30,40);
+  box26 = new Box(715,150,30,40);
+  box27 = new Box(695,130,30,40);
+  box28 = new Box(705,130,30,40);
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new Slingshot(this.ball,{x:100,y:200});
+
+}
+
+function draw() {
+  background(255,255,255);
+  text("score"+score ,20,30);
+  ground.display(); 
+  drawSprites();
+  stand1.display();
+  stand2.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box7.display();
+  box8.display();
+  box9.display();
+  box10.display();
+  box11.display();
+  box12.display();
+  box13.display();
+  box14.display();
+  box15.display();
+  box16.display();
+  box17.display();
+  box18.display();
+  box19.display();
+  box20.display();
+  box21.display();
+  box22.display();
+  box23.display();
+  box24.display();
+  box25.display();
+  box26.display();
+  box27.display();
+  box28.display();
+  imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
+    
+  slingShot.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
+}
+function keyPressed(){
+  if(keyCode === 32){
+      slingShot.attach(this.polygon);
+  }
+}
